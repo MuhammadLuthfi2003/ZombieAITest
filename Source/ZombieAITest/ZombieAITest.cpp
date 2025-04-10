e49779b228aad77b6aa0225393ee386ddd6f9850
MuhammadLuthfi2003 <88194674+MuhammadLuthfi2003@users.noreply.github.com>
2025-04-10 15:32:13 +0700

    coba add .cpp file

---
+++ Source/ZombieAITest/ZombieAITest.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ZombieAITest.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ZombieAITest, "ZombieAITest" );
